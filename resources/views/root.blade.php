e23c4200e06b9e3b469f996d2dec665509bd17c8
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2023-05-30 14:35:02 +0200

    Feature
    feature! favicon and app title

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Auth.Test Package</title>
 
         @spladeHead
         @vite('resources/js/app.js')
